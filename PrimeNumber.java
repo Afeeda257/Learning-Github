ac51c1cb55943c5e6010aff03944fda5ca9220ea
Aniket Karmakar <Aniketkarmakar89@gmail.com>
2021-01-03 14:51:04 +0530

    Some modification has been done

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
-// Made some chnages... Added comment
+// Made some changes... Added comment
 public class PrimeNumber {
 	public static void main(String args[])
 	{
 		int num,i,j,k;
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter a number:");
-		num=sc.nextInt();
+		num=sc.nextLine();
 		
 		for(i=2;i<num;i++)
 		{
 			k=0;
 			for(j=2;j<i;j++)
 				{
-					if(i%j==0)
+					if(i%j == 0)
 						k=1;
 				}
 				if (k==0)
